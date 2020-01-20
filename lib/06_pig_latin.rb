647f232c7dc5586d63ca98d91b484f67930a287b
BearCub29 <poulichet.florian@gmail.com>
2020-01-20 22:50:45 +0100

    second version

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,5 +1,14 @@
-def translate ()
+def translate (word)
 
+if  word.start_with?(2*(aeiouy)) 
+    return word + "ay"
+elsif word.start_with?(3*(aeiouy)) 
 
+    return word.reverse + "ay"
+end
 
 end
+
+translate ("banana")
+
+#aBcDeF".casecmp?("abcde") 
